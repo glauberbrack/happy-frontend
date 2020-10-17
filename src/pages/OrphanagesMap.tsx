77e82d939ad38b2364d83ada94411f4a151070ab
glauberbrack <glauber@brack.com.br>
2020-10-17 15:06:55 -0300

    feat: create orphanagesmap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
+import '../styles/pages/orphanages-map.css'
+
+import mapMarkerImg from '../assets/map-marker.svg'
 
 function OrphanagesMap() {
     return (
-        <h1>Orphanages</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy Location"/>
+                    <h2>Choose an orphanage</h2>
+                    <p>A lot of kids are waiting for you (:</p>
+                </header>
+
+                <footer>
+                    <strong>Bebedouro</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div>mapinha</div>
+            <Link to="#" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     )
 }
 
