49bd16b321a80d23f2856a81fd46b6fe50de82b7
glauberbrack <glauber@brack.com.br>
2020-10-17 15:24:05 -0300

    importing map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup }  from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css'
 
@@ -22,7 +25,16 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div>mapinha</div>
+            <Map
+                center={[-22.872064,-43.3717248]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAP_TOKEN}`} 
+                />
+                </Map>
+                
             <Link to="#" className="create-orphanage">
                 <FiPlus size={32} color="#FFF"/>
             </Link>
