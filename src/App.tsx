59769551db8cd500711d6609091138126f446409
glauberbrack <glauber@brack.com.br>
2020-10-17 01:44:19 -0300

    feat(landing): create css and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Oizinhuuu
-        </p>
-        
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logo} alt="Happy"/>
+
+        <main>
+          <h1>Bring happiness to the world!</h1>
+          <p>Visit orphanages and change children's day</p>
+        </main>
+
+        <div className="location">
+          <strong>Bebedouro</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="#" className='enter-app'> > </a>
+      </div>
     </div>
   );
 }
